98a434265ce09800a917cb4fc2753f7eacd3870d
OhSeungAh2 <pppp9944@naver.com>
2026-09-01 15:39:55 +0900

    구조동물(보호중이에요)조회 및 필터링

--- Server/src/modules/rescue-animals/rescue-animals.controller.js
+++ Server/src/modules/rescue-animals/rescue-animals.controller.js
@@ -1,22 +1,38 @@
 import * as service from "./rescue-animals.service.js"
-import { ok, created, fail } from "../../utils/response.js"
+import { ok, fail } from "../../utils/response.js"
 
 // 5.1 구조동물 목록 조회 (필터링)
 export async function getAnimals(req, res, next) {
-  try {
-    // TODO: 필터 + 페이지네이션, notice_edt 지난 공고는 제외
-    fail(res, 501, "NOT_IMPLEMENTED", "아직 구현되지 않았습니다.")
-  } catch (err) {
-    next(err)
-  }
+    try {
+        const result = await service.getAnimals(req.query)
+
+        ok(res, result)
+
+    } catch (err) {
+        next(err)
+    }
 }
 
 // 5.2 구조동물 상세 조회
 export async function getAnimal(req, res, next) {
-  try {
-    // TODO: 보호소 정보, 공고 기간, is_ending_soon 포함해서 반환
-    fail(res, 501, "NOT_IMPLEMENTED", "아직 구현되지 않았습니다.")
-  } catch (err) {
-    next(err)
-  }
+    try {
+        const animal = await service.getAnimal(
+            req.params.desertionNo,
+            req.userId ?? null
+        )
+
+        if (!animal) {
+            return fail(
+                res,
+				404,
+                "RESCUE_ANIMAL_NOT_FOUND",
+                "구조동물 공고를 찾을 수 없습니다."
+            )
+        }
+
+        ok(res, animal)
+
+    } catch (err) {
+        next(err)
+    }
 }
--- Server/src/modules/rescue-animals/rescue-animals.router.js
+++ Server/src/modules/rescue-animals/rescue-animals.router.js
@@ -1,9 +1,10 @@
 import express from "express"
 import * as controller from "./rescue-animals.controller.js"
+import { optionalAuth } from "../../middleware/auth.middleware.js"
 
 const router = express.Router()
 
 router.get("/", controller.getAnimals)             // 5.1 구조동물 목록 조회 (필터링)
-router.get("/:desertionNo", controller.getAnimal)  // 5.2 구조동물 상세 조회
+router.get("/:desertionNo", optionalAuth, controller.getAnimal)  // 5.2 구조동물 상세 조회
 
 export default router
