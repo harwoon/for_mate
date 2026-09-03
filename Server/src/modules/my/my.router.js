f6bd0ff11a196406ab18b2d700d2a4810f50b511
OhSeungAh2 <pppp9944@naver.com>
2026-09-03 17:20:57 +0900

    백엔드: 마에피이지

--- Server/src/modules/my/my.router.js
+++ Server/src/modules/my/my.router.js
@@ -4,7 +4,13 @@ import * as controller from "./my.controller.js"
 
 const router = express.Router()
 
-router.get("/lost-posts", requireAuth, controller.getMyLostPosts)    // 8.1 내 실종 신고 목록
-router.get("/found-posts", requireAuth, controller.getMyFoundPosts)  // 8.2 내 발견제보 목록
+// 8.1 마이페이지 요약 조회
+router.get("/summary", requireAuth, controller.getSummary)
+
+// 8.2 내 실종 공고 목록 조회
+router.get("/lost-posts", requireAuth, controller.getMyLostPosts) 
+
+// 8.3 내 발견제보 목록 조회
+router.get("/found-posts", requireAuth, controller.getMyFoundPosts) 
 
 export default router
